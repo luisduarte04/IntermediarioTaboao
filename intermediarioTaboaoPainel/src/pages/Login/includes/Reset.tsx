97eeb18f595b5b7249ce9911cf8f40236cb2a0a8
Luis Guilherme <lgduarte.santos2@hotmail.com>
2025-06-17 15:09:39 -0300

    Criado tela de login e recuperar senha, com img de fundo

---
+++ intermediarioTaboaoPainel/src/pages/Login/includes/Reset.tsx
@@ -0,0 +1,29 @@
+import { Box, Card, Stack, TextField, Button, Typography } from '@mui/material'
+import {Link} from 'react-router-dom';
+import React from 'react'
+
+
+
+
+export default function Reset() {
+  return (
+    <Box sx={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#dbdbdb' , backgroundImage: 'url("/images.jpg")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'}}>
+            
+      <Card sx={{ width: 400, padding: 3 }}>
+        <Stack spacing={2}>
+          
+          <Typography variant="h6" align='center' padding={3}>RESETAR SENHA</Typography>
+          <Typography  align='center' padding={1}>Digite seu email para receber um link de redefinição de senha:</Typography>
+          <TextField label="email" align='center' >Digite seu email para receber um link de redefinição de senha</TextField>
+          <Typography  align="center" component={Link} to="/">Voltar a tela de Login</Typography>
+          <Button variant="contained" sx={{ backgroundColor: '#f8240f', '&:hover': { backgroundColor: '#a01208' } }} fullWidth>Enviar Email</Button>
+        </Stack>
+      </Card>
+    </Box>
+    
+
+  )
+}
--- intermediarioTaboaoPainel/src/pages/Login/index.tsx
+++ intermediarioTaboaoPainel/src/pages/Login/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import LoginForm from './includes/Login';
+import Reset from './includes/Reset';
 
 const Login = () => {
   return (
     <div>
       <LoginForm />
+      
     </div>
   );
 };
 
+const ResetPage = () => {
+  return (
+    <div>
+      <Reset />
+    </div>
+  );
+}
 export default Login;
