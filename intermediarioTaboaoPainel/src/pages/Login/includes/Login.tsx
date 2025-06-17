699da9fccad849c40e1e0497d946a86de6e36f70
Luis Guilherme <lgduarte.santos2@hotmail.com>
2025-06-17 11:18:31 -0300

    Criação dos campos

--- intermediarioTaboaoPainel/src/pages/Login/includes/Login.tsx
+++ intermediarioTaboaoPainel/src/pages/Login/includes/Login.tsx
@@ -1,10 +1,18 @@
-import { Card  } from '@mui/material'
+import { Box, Card, Grid, Stack, TextField, Button, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Login() {
   return (
-    <div>
-      Login
-    </div>
+    <Box sx={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Card sx={{ width: 400, padding: 3 }}>
+        <Stack spacing={2}>
+          <Typography variant="h5" align="center">Login</Typography>
+          <TextField label="Email" variant="outlined" fullWidth />
+          <TextField label="Senha" type="password" variant="outlined" fullWidth />
+          <Button variant="contained" color="primary" fullWidth>Entrar</Button>
+        </Stack>
+      </Card>
+    </Box>
+    
   )
 }
